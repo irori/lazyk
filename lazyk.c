6cb8dbf26221e902c8c315de791af4d2b1ffa584
irori <irorin@gmail.com>
2019-08-24 11:06:53 +0900

    Relicense to MIT

--- lazyk.c
+++ lazyk.c
@@ -2,9 +2,7 @@
  *  Lazy K interpreter
  *
  *  Copyright 2008 irori <irorin@gmail.com>
- *  This is free software. You may modify and/or distribute it under the
- *  terms of the GNU General Public License, version 2 or any later version.
- *  It comes with no warranty.
+ *  This code is licensed under the MIT License (see LICENSE file for details).
  */
 #include <stdio.h>
 #include <string.h>
